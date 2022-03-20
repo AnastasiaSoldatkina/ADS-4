e6fcb358399dffce8853471030b66c6aa3f6d680
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-03-20 17:54:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,116 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+int i = 0;
+    int w = 0;
+    int G = 0;
+    while (i < len) {
+        w = i+1;
+        while (w < len) {
+            if (arr[i] + arr[w] == value) {
+                G++;
+            }
+            w++;
+        }
+        i++;
+    }
+    return G;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+int i = 1;
+    int r = 0;
+    int Z = 0;
+    if (arr[0] > arr[1]) {
+        r = arr[1];
+        arr[1] = arr[0];
+        arr[0] = r;
+    }
+    while (i < len-1) {
+        while (arr[i] > arr[i + 1]) {
+            r = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = r;
+            i--;
+        }
+        i++;
+    }
+    i = 0;
+    int a = len - 1;
+    while (i < a) {
+        while ((arr[i] + arr[a] > value) && (a>i)) {
+            a--;
+        }
+        while ((arr[i] + arr[a] == value) && (a > i)) {
+            Z++;
+            a--;
+        }
+        i++;
+        a = len-1;
+    }
+    return Z;
+}
+int cbinsearch(int* arr, int size, int value) {
+int i = 0;
+    int j = size;
+    int d = 0;
+    int Res = 0;
+    while (i < j) {
+        int x = i + (j - i) / 2;
+        if (i == j - 1) {
+            break;
+        }
+        d = x;
+        if (arr[x] == value) {
+            while (arr[x] == value) {
+                x++;
+            }
+            j = x;
+            x = d;
+            while (arr[x] == value) {
+                x--;
+            }
+            i = x;
+            Res = j - i - 1;
+            break;
+        }
+        if (arr[x] < value) {
+            i = x;
+            x++;
+        }
+        else {
+            j = x-1;
+            x--;
+        }
+    }
+    return Res;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+int i = 1;
+    int r = 0;
+    int c = 0;
+    int Con = 0;
+    int min = 0;
+    if (arr[0] > arr[1]) {
+        r = arr[1];
+        arr[1] = arr[0];
+        arr[0] = r;
+    }
+    while (i < len - 1) {
+        while (arr[i] > arr[i + 1]) {
+            r = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = r;
+            i--;
+        }
+        i++;
+    }
+    while (arr[c] < value - arr[c]) {
+        min = value - arr[c];
+        Con = Con + cbinsearch(arr, len, min);
+        c++;
+    }
+    if (value % 2 == 0) {
+        min = value / 2;
+        Con = Con + cbinsearch(arr, len, min);
+    }
+    return Con;
 }
